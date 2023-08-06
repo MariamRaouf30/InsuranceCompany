5e7cdaae3b6e0675a931feeb6b90da01e403fa2a
MariamRaouf30 <mariam.shenouda303@gmail.com>
2023-08-06 16:36:42 +0300

    Update: Customer Model and application.properties

--- Backend/CreateAccount/src/main/java/com/example/CreateAccount/model/Customer.java
+++ Backend/CreateAccount/src/main/java/com/example/CreateAccount/model/Customer.java
@@ -1,10 +1,11 @@
 package com.example.CreateAccount.model;
 //import org.springframework.cglib.core.Local;
-import org.springframework.data.annotation.Id;
 //import org.springframework.data.mongodb.core.mapping.Document;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 import java.time.LocalDate;
@@ -14,13 +15,9 @@ import java.time.LocalDate;
 @Table(name = "customers")
 public class Customer {
     @Id
-    @Column
-    private String id;
-    @Column
+    private int id;
     private String name;
-    @Column
     private String password;
-    @Column
     private String phone_number;
     private LocalDate date;
 }
--- Backend/CreateAccount/src/main/java/com/example/CreateAccount/repository/CustomerRepository.java
+++ Backend/CreateAccount/src/main/java/com/example/CreateAccount/repository/CustomerRepository.java
@@ -1,12 +1,13 @@
 package com.example.CreateAccount.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 //import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.CreateAccount.model.Customer;
 
 @Repository
-public interface CustomerRepository extends CrudRepository<Customer,String>{
+public interface CustomerRepository extends JpaRepository<Customer,Integer>{
 
 }

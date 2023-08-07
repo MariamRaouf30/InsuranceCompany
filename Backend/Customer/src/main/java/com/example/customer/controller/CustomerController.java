5d5397d4845a48fac505a63e528d8579870685c1
MariamRaouf30 <mariam.shenouda303@gmail.com>
2023-08-07 12:29:19 +0300

    Updates: swagger dependencies

--- Backend/Customer/src/main/java/com/example/customer/controller/CustomerController.java
+++ Backend/Customer/src/main/java/com/example/customer/controller/CustomerController.java
@@ -2,16 +2,17 @@ package com.example.customer.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.customer.model.Customer;
 import com.example.customer.repository.CustomerRepository;
+
+
+
 
 @RestController
 public class CustomerController {
